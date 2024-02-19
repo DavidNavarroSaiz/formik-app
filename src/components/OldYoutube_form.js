37d6e6166f55651c7db96bfc012ba0662c4f10fe
DavidNavarroSaiz <dcnavarros@unal.edu.co>
2024-02-19 14:21:04 -0500

    reducing boilerplate

--- src/components/OldYoutube_form.js
+++ src/components/OldYoutube_form.js
@@ -11,6 +11,22 @@ const initialValues = {
 const onSubmit = values => {
   console.log(values)
 }
+const validate =values =>{
+  let errors = {}
+  if (!values.name){
+    errors.name = 'Required'
+  }
+  if (!values.email){
+    errors.email = 'Required'
+  }
+  else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+    errors.email = 'Invalid email format';
+  }
+  if (!values.channel){
+    errors.channel = 'Required'
+  }
+  return errors
+}
 
 const validationSchema = Yup.object({
   name:Yup.string().required("Required"),
@@ -30,17 +46,17 @@ const validationSchema = Yup.object({
       <form onSubmit={formik.handleSubmit}>
         <div className='form-control'>
         <label htmlFor='name'>Name</label>
-        <input type='text' id='name' name='name' {...formik.getFieldProps('name')} ></input>
+        <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} onBlur={formik.handleBlur}></input>
         {formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
         </div>
         <div className='form-control'>
         <label htmlFor='name'>Email</label>
-        <input type='text' id='email' name='email' {...formik.getFieldProps('email')}></input>
+        <input type='text' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} onBlur={formik.handleBlur}></input>
         {formik.touched.email && formik.errors.email ? <div className='error'>{formik.errors.email}</div> : null}
         </div>
         <div className='form-control'>
         <label htmlFor='name'>Channel</label>
-        <input type='text' id='channel' name='channel' {...formik.getFieldProps('channel')}></input>
+        <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel} onBlur={formik.handleBlur}></input>
         {formik.touched.channel && formik.errors.channel ? <div className='error'>{formik.errors.channel}</div> : null}
         </div>
         <button type='submit'>Submit</button>
